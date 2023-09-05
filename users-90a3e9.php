03c539f7dc6a34ce631e0bcba18f66ebb5fb481d
BerkleeCollegeofMusicUndergraduateGrade <143133905+BerkleeCollegeofMusicUndergraduateGrade@users.noreply.github.com>
2023-09-05 16:30:25 -0400

    Add files via upload

---
+++ users-90a3e9.php
@@ -0,0 +1,3 @@
+<?php
+    $users = array('iager@berklee.edu' => array('id' => 1, 'name' => 'Imani', 'surname' => 'Ager', 'pwd' => 'eQUJ/NPbQ5m.TrKgCGCpASL1qg7M4tl2', 'email' => 'iager@berklee.edu', 'info' => '', 'groups' => array('0')));
+    $salt = '$2y$10$q2mebebwyxOla0ikqVulqo$';
